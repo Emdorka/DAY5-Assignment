d262374120b73d4816d9c98cb8d8c018887c7c56
Emdorka <emdorkasyiem1@gmail.com>
2022-12-05 17:48:01 +0530

    first commit

---
+++ word.sh
@@ -0,0 +1,15 @@
+echo "enter the number"
+read numb
+case $numb in
+ 0) echo "number in word is Zero" ;;
+ 1) echo  "number in word is one" ;;
+ 2) echo "number in word is two" ;;
+ 3) echo "number in word is three" ;;
+ 4) echo "number in word is four" ;;
+ 5) echo "number in word is five" ;;
+ 6) echo "number in word is six" ;;
+ 7) echo "number in word is seven" ;;
+ 8) echo "number in word is eight" ;;
+ 9) echo "number in word is nine" ;;
+ *) echo "the number is from 0 t o 9"
+esac
