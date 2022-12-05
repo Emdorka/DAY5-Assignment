384880f55e2f1cbe2eeb8d2291291fb547baf918
Emdorka <emdorkasyiem1@gmail.com>
2022-12-05 18:36:28 +0530

    adding files

---
+++ num.sh
@@ -0,0 +1,17 @@
+echo "enter the number"
+read num
+case $num in
+  1)echo "MOnday" ;;
+  2)echo "Tuesday" ;;
+  3)echo "Wednesday" ;;
+  4)echo "Thursday" ;;
+  5)echo "Friday" ;;
+  6)echo "Saturday" ;;
+  7)echo "Sunday" ;;
+  *)echo "the number is between 1 to 7" ;;
+
+esac
+
+
+
+
